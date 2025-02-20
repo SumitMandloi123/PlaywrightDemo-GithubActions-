3c3eb255925cdce38868e4eaa2b48a866cacabe9
Sumit Mandloi <sumit.mandloi@unthinkable.co>
2025-02-20 14:01:55 +0530

    second commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  testMatch : ["**.test.ts"], 
+  // testMatch : ["**.test.ts"], 
 
   /* Configure projects for major browsers */
   projects: [
